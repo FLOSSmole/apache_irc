79d501aba4c93036b99862bb65c2d9d5c0072729
evan-ashwell <eashwell2@elon.edu>
2016-06-06 13:26:04 -0400

    Update 2parseActiveMQIRCLogs.pl
    Changed formatting of the code and changed a few variables.

--- 2parseActiveMQIRCLogs.pl
+++ 2parseActiveMQIRCLogs.pl
@@ -4,6 +4,8 @@
 ## Please see the Perl Artistic License 2.0.
 ## 
 ## Copyright (C) 2004-2015 Megan Squire <msquire@elon.edu>
+## Contributions from:
+## Evan Ashwell - converted from perl to python
 ##
 ## We're working on this at http://flossmole.org - Come help us build 
 ## an open and accessible repository for data and analyses for open
@@ -18,141 +20,21 @@
 ##
 ## and
 ##
-## FLOSSmole (2004-2015) FLOSSmole: a project to provide academic access to data 
+## FLOSSmole (2004-2016) FLOSSmole: a project to provide academic access to data 
 ## and analyses of open source projects.  Available at http://flossmole.org 
-#
-# --------------------------------------------------
-# subroutine: parseFile
-# takes: two database connections (local and remote) and a datasource_id
-# purpose:
-# --get each file on disk
-# --pull out the lines
-# --parse out the pieces of the lines
-# --write each line to the irc table in both local and remote db
-# --------------------------------------------------
-def parseFile(dbh2, dbh3, ds, fileLoc):
-    p_dbh2  = dbh2
-    p_dbh3  = dbh3
-    p_ds    = ds
-    p_fileLoc = fileLoc
-    cursor= dbh2.cursor()
-    #date is in the filename, in the format:
-    # 48039/20140306
-    datelog =""
-    formatting= re.search("^(.*?)\/(.*?)$",p_fileLoc)
-    if formatting:
-        tempdate = formatting.group(2)
-        print ("got ", tempdate," for date");
-        
-        dateCorrection= re.search("^(\d\d\d\d)(\d\d)(\d\d)",tempdate)
-        if dateCorrection:
-            datelog = dateCorrection.group(1) + "-" + dateCorrection.group(2) + "-" + dateCorrection.group(3)
-            fileLoc= formatting.group(1)+ "/" + datelog
-        else:
-            datelog=formatting.group(2)
-    # open the file
-    print ("opening file: " + p_fileLoc );
-
-    try:
-        log = codecs.open(fileLoc, 'r', encoding='utf-8', errors='ignore')
-        line=log.read()
-        line= line[2:]
-        line= line[:-3]
-        log=line.split("\\n")
-    except pymysql.Error as err:
-        print(err)
-    print(log)
-    
-    linenum = 0
-    for line in log:   
-        print(line)
-        linenum= linenum+1
-        send_user = ""
-        timelog = ""
-        line_message = ""
-        messageType = ""
-        
-        # parse out rest of details & insert
-        # 1. get system message vs regular message, parse
-        # 2. insert
-        #
-        # here are the two patterns:
-        # [20:05] <zmhassan> any tips would be gladly appreciated
-        # [16:09] *** jbonofre has quit IRC (Excess Flood)
-        regularMessage= re.search('^\[(.*?)\]\s+\<(.*?)\>\s+(.*?)$',line)
-        systemMessage= re.search('^\[(.*?)\]\s+\*\*\*\s+(.*?)$',line)
-        if regularMessage: #regular message
-            timelog = regularMessage.group(1)
-            send_user = regularMessage.group(2)
-            line_message = regularMessage.group(3)
-            messageType = "message";
-        elif systemMessage: # system message
-            messageType = "system";
-            timelog = systemMessage.group(1)
-            line_message = systemMessage.group(2)
-    	
-        print("inserting row ", linenum," for ", datelog)
-
-        #======
-        # LOCAL
-        #======                
-        if ((p_ds) and (messageType != "")):
-            try:
-                insertQuery= "INSERT INTO `apache_activemq_irc`(`datasource_id`,\
-                `line_num`,\
-                `full_line_text`,\
-                `line_message`,\
-                `date_of_entry`,\
-                `time_of_entry`\
-                ,`type`\
-                ,`send_user`\
-                ,`last_updated`) VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s)"
-                cursor.execute(insertQuery,(p_ds,linenum,line,line_message,datelog,timelog, messageType, send_user,datetime.datetime.now()))
-                dbh2.commit()
-            except pymysql.Error as error:
-                print(error)
-                dbh2.rollback()
-    cursor.close()
-
-"""
-        #======
-        # SYR
-        #======
-        if (($p_ds) && ($type ne ""))
-        {
-            my $insert3 = $p_dbh3->prepare(qq{
-                            INSERT IGNORE INTO apache_activemq_irc
-                                (datasource_id, 
-                                line_num,
-                                full_line_text,
-                                line_message,
-                                date_of_entry,
-                                time_of_entry,
-                                type,
-                                send_user,
-                                last_updated) 
-                            VALUES (?,?,?,?,?,?,?,?,NOW())
-                            });
-            $insert3->execute($p_ds, $linenum, $line, $line_message, $datelog, $timelog, $type, $send_user)
-                or die "Couldn't execute statement on SYR: " . $insert3->errstr;
-            $insert3->finish();
-        } 
-    }
-}
-"""	
 ################################################################
 # usage:
-# > perl 2parseActiveMQIRCLogs.pl <datasource_id> 
+# > python3 2parseActiveMQIRCLogs.pl <datasource_id><password> 
 #
 # example usage:
-# > perl 2parseActiveMQIRCLogs.pl 48483 
+# > python3 2parseActiveMQIRCLogs.pl 62204 password 
 #
 # purpose: 
 # open each IRC log in the directory, parse out the interesting bits
-# notes:
-# START WITH THE FIRST DS IN THE DB YOU ARE INTERESTED IN
-# Does not need to be the latest one
-#
+# save to database table
+# 
+# each date = one datasource_id
+# each line is incremented with a number for that date
 ################################################################
 import pymysql
 import sys
@@ -163,12 +45,12 @@ import datetime
 datasource_id = str(sys.argv[1])
 password= str(sys.argv[2])
 forge_id = 36
-
-if (datasource_id):
+newDS= datasource_id
+if datasource_id and password:
 	# connect to db (once at local grid6, and once at Syracuse)
 	# dsn takes the format of "DBI:mysql:ossmole_merged:grid6.cs.elon.edu"
     try:
-        dbh2 = pymysql.connect(host='grid6.cs.elon.edu',
+        db1 = pymysql.connect(host='grid6.cs.elon.edu',
                                   database='test',
                                   user='eashwell',
                                   password=password,
@@ -177,7 +59,7 @@ if (datasource_id):
     except pymysql.Error as err:
         print(err)
     try:
-        dbh3 = pymysql.connect(host='grid6.cs.elon.edu',
+        db2 = pymysql.connect(host='grid6.cs.elon.edu',
                                   database='test',
                                   user='eashwell',
                                   password=password,
@@ -185,38 +67,146 @@ if (datasource_id):
     
     except pymysql.Error as err:
         print(err)
-        """
+        
     try:
-         dbh3 = pymysql.connect(host='flossdata.syr.edu',
+         db3 = pymysql.connect(host='flossdata.syr.edu',
                                   database='rubygems',
                                   user='megan',
                                   password=password,
                                   charset='utf8')
     except pymysql.Error as err:
         print(err)
-        dbh2= "remote"
-        """
-    cursor= dbh2.cursor()
+    
+    cursor1= db1.cursor()
+    cursor2= db2.cursor()
+    cursor3= db3.cursor()
     
     selectQuery="select datasource_id, comments \
 		from test.datasources \
 		where datasource_id >= %s \
 		and forge_id=%s"  
-    cursor.execute(selectQuery,(datasource_id,forge_id))
-    rows=cursor.fetchall()    
+    cursor1.execute(selectQuery,(datasource_id,forge_id))
+    rows=cursor1.fetchall()    
     
     print(rows)
      
     for row in rows : 
-        ds= row[0]
+        newDS= row[0]
         fileLoc= row[1]
         print ("==================\n")
-        parseFile(dbh2, dbh3, ds, fileLoc)
-    cursor.close()
-    dbh2.close()
-    dbh3.close()
-    print("done")
-else:
-	print ("You need both a datasource_id and a date to start on your commandline.")
-	exit;
+        
+        #date is in the filename, in the format:
+        # 48039/20140306
+        datelog =""
+        formatting= re.search("^(.*?)\/(.*?)$",fileLoc)
+        if formatting:
+            tempdate = formatting.group(2)
+            correctForm= re.search("^(\d\d\d\d)(\d\d)(\d\d)",tempdate)
+            if correctForm:
+                datelog = correctForm.group(1) + "-" + correctForm.group(2) + "-" + correctForm.group(3)
+                fileLoc= formatting.group(1)+ "/" + datelog
+            else:
+                datelog=formatting.group(2)
+        
+        # open the file
+        print("working on: ", fileLoc)
+        log = codecs.open(fileLoc, 'r', encoding='utf-8', errors='ignore')
+        line=log.read()
+        line= line[2:]
+        line= line[:-3]
+        log=line.split("\\n")
+        
+        
+        
+        # Parse out details
+        linenum = 0
+        for line in log:   
+            print(line)
+            linenum= linenum+1
+            send_user = ""
+            timelog = ""
+            line_message = ""
+            messageType = ""
+        
+            # parse out rest of details & insert
+            # 1. get system message vs regular message, parse
+            # 2. insert
+            #
+            # here are the two patterns:
+            # [20:05] <zmhassan> any tips would be gladly appreciated
+            # [16:09] *** jbonofre has quit IRC (Excess Flood)
+            regularMessage= re.search('^\[(.*?)\]\s+\<(.*?)\>\s+(.*?)$',line)
+            systemMessage= re.search('^\[(.*?)\]\s+\*\*\*\s+(.*?)$',line)
+            if regularMessage: #regular message
+                timelog = regularMessage.group(1)
+                send_user = regularMessage.group(2)
+                line_message = regularMessage.group(3)
+                messageType = "message";
+            elif systemMessage: # system message
+                messageType = "system";
+                timelog = systemMessage.group(1)
+                line_message = systemMessage.group(2)
+            
 
+            if ((datasource_id) and (messageType != "")):
+                insertQuery= "INSERT INTO `apache_activemq_irc`(`datasource_id`,\
+                              line_num,\
+                              full_line_text,\
+                              line_message,\
+                              date_of_entry,\
+                              time_of_entry\
+                              ,type\
+                              ,send_user\
+                              ,last_updated) VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s)"
+                currDate= datetime.datetime.now()
+                
+                #======
+                # LOCAL
+                #======                
+                try:    
+                    cursor2.execute(insertQuery,
+                                   (newDS,
+                                    linenum,
+                                    line,
+                                    line_message,
+                                    datelog,
+                                    timelog, 
+                                    messageType, 
+                                    send_user,
+                                    currDate))
+                    db2.commit()
+                except pymysql.Error as error:
+                    print(error)
+                    db2.rollback()
+                
+                #======
+                # REMOTE
+                #======
+
+                try:    
+                    cursor3.execute(insertQuery,
+                                   (newDS,
+                                    linenum,
+                                    line,
+                                    line_message,
+                                    datelog,
+                                    timelog, 
+                                    messageType, 
+                                    send_user,
+                                    currDate))
+                    db3.commit()
+                except pymysql.Error as error:
+                    print(error)
+                    db3.rollback()
+                   
+    cursor1.close()
+    cursor2.close()
+    cursor3.close()
+    
+    db1.close()
+    db2.close()
+    db3.close()
+
+else:
+	print ("You need both a datasource_id and a password to start on your commandline.")
+	exit;
