e77edb8d783be544b66b54debed67ba66593a0a7
Megan Squire <megansquire@gmail.com>
2016-06-07 11:27:31 -0400

    Update 2parseActiveMQIRCLogs.pl

--- 2parseActiveMQIRCLogs.pl
+++ 2parseActiveMQIRCLogs.pl
@@ -1,9 +1,9 @@
-#!/usr/bin/perl
+# -*- coding: utf-8 -*-
 ## This program is free software; you can redistribute it
 ## and/or modify it under the same terms as Perl itself.
 ## Please see the Perl Artistic License 2.0.
 ## 
-## Copyright (C) 2004-2015 Megan Squire <msquire@elon.edu>
+## Copyright (C) 2004-2016 Megan Squire <msquire@elon.edu>
 ## Contributions from:
 ## Evan Ashwell - converted from perl to python
 ##
@@ -24,10 +24,10 @@
 ## and analyses of open source projects.  Available at http://flossmole.org 
 ################################################################
 # usage:
-# > python3 2parseActiveMQIRCLogs.pl <datasource_id><password> 
+# > python3 2parseActiveMQIRCLogs.py <datasource_id><password> 
 #
 # example usage:
-# > python3 2parseActiveMQIRCLogs.pl 62204 password 
+# > python3 2parseActiveMQIRCLogs.py 62204 password 
 #
 # purpose: 
 # open each IRC log in the directory, parse out the interesting bits
@@ -42,27 +42,30 @@ import re
 import codecs
 import datetime
 
-datasource_id = str(sys.argv[1])
-password= str(sys.argv[2])
-forge_id = 36
-newDS= datasource_id
+datasource_id = sys.argv[1]
+password      = sys.argv[2]
+forgeID       = 36
+
 if datasource_id and password:
 	# connect to db (once at local grid6, and once at Syracuse)
 	# dsn takes the format of "DBI:mysql:ossmole_merged:grid6.cs.elon.edu"
     try:
         db1 = pymysql.connect(host='grid6.cs.elon.edu',
-                                  database='test',
-                                  user='eashwell',
+                                  database='ossmole_merged',
+                                  user='megan',
                                   password=password,
+                                  use_unicode=True,
                                   charset='utf8')
     
     except pymysql.Error as err:
         print(err)
+    
     try:
         db2 = pymysql.connect(host='grid6.cs.elon.edu',
-                                  database='test',
-                                  user='eashwell',
+                                  database='apache_irc',
+                                  user='megan',
                                   password=password,
+                                  use_unicode=True,
                                   charset='utf8')
     
     except pymysql.Error as err:
@@ -70,64 +73,61 @@ if datasource_id and password:
         
     try:
          db3 = pymysql.connect(host='flossdata.syr.edu',
-                                  database='rubygems',
+                                  database='apache_irc',
                                   user='megan',
                                   password=password,
+                                  use_unicode=True,
                                   charset='utf8')
     except pymysql.Error as err:
         print(err)
     
-    cursor1= db1.cursor()
-    cursor2= db2.cursor()
-    cursor3= db3.cursor()
+    cursor1 = db1.cursor()
+    cursor2 = db2.cursor()
+    cursor3 = db3.cursor()
     
     selectQuery="select datasource_id, comments \
-		from test.datasources \
-		where datasource_id >= %s \
-		and forge_id=%s"  
-    cursor1.execute(selectQuery,(datasource_id,forge_id))
-    rows=cursor1.fetchall()    
-    
-    print(rows)
-     
-    for row in rows : 
-        newDS= row[0]
-        fileLoc= row[1]
-        print ("==================\n")
+        		from datasources \
+        		where datasource_id >= %s \
+        		and forge_id=%s"  
+    cursor1.execute(selectQuery,(datasource_id,forgeID))
+    rows = cursor1.fetchall()    
         
-        #date is in the filename, in the format:
-        # 48039/20140306
-        datelog =""
-        formatting= re.search("^(.*?)\/(.*?)$",fileLoc)
-        if formatting:
-            tempdate = formatting.group(2)
-            correctForm= re.search("^(\d\d\d\d)(\d\d)(\d\d)",tempdate)
-            if correctForm:
-                datelog = correctForm.group(1) + "-" + correctForm.group(2) + "-" + correctForm.group(3)
-                fileLoc= formatting.group(1)+ "/" + datelog
-            else:
-                datelog=formatting.group(2)
+    for row in rows : 
+        newDS = int(row[0])
+        fileLoc = row[1]
+        print ("==================\n")
         
         # open the file
         print("working on: ", fileLoc)
-        log = codecs.open(fileLoc, 'r', encoding='utf-8', errors='ignore')
-        line=log.read()
-        line= line[2:]
-        line= line[:-3]
-        log=line.split("\\n")
-        
-        
+        log  = codecs.open(fileLoc, 'r', encoding='utf-8', errors='ignore')
+        line = log.read()
+        line = line[2:]
+        line = line[:-3]
+        log  = line.split("\\n")
         
         # Parse out details
         linenum = 0
-        for line in log:   
-            print(line)
-            linenum= linenum+1
-            send_user = ""
-            timelog = ""
+        for line in log:
+            linenum     += 1
+            send_user    = ""
+            timelog      = ""
             line_message = ""
-            messageType = ""
+            messageType  = ""
         
+        
+            #date is in the filename, in the format:
+            # 48039/20140306
+            datelog =""
+            formatting= re.search("^(.*?)\/(.*?)$",fileLoc)
+            if formatting:
+                tempdate = formatting.group(2)
+                correctForm= re.search("^(\d\d\d\d)(\d\d)(\d\d)",tempdate)
+                if correctForm:
+                    datelog = correctForm.group(1) + "-" + correctForm.group(2) + "-" + correctForm.group(3)
+                    #fileLoc= formatting.group(1)+ "/" + datelog
+                else:
+                    datelog=formatting.group(2)
+                    
             # parse out rest of details & insert
             # 1. get system message vs regular message, parse
             # 2. insert
@@ -149,16 +149,16 @@ if datasource_id and password:
             
 
             if ((datasource_id) and (messageType != "")):
-                insertQuery= "INSERT INTO `apache_activemq_irc`(`datasource_id`,\
+                insertQuery= "INSERT IGNORE INTO apache_activemq_irc(datasource_id,\
                               line_num,\
                               full_line_text,\
                               line_message,\
                               date_of_entry,\
-                              time_of_entry\
-                              ,type\
-                              ,send_user\
-                              ,last_updated) VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s)"
-                currDate= datetime.datetime.now()
+                              time_of_entry,\
+                              type,\
+                              send_user,\
+                              last_updated) VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s)"
+                currDate = datetime.datetime.now()
                 
                 #======
                 # LOCAL
@@ -209,4 +209,3 @@ if datasource_id and password:
 
 else:
 	print ("You need both a datasource_id and a password to start on your commandline.")
-	exit;
